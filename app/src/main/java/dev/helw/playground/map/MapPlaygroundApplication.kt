2c399e7a4ce80ad8a1320965f86f50ac11e342d9
Ahmed El-Helw <ahmedre@gmail.com>
2025-12-07 20:51:33 +0400

    Rename application subclass to be less confusing

--- app/src/main/java/dev/helw/playground/map/MapPlaygroundApplication.kt
+++ app/src/main/java/dev/helw/playground/map/MapPlaygroundApplication.kt
@@ -3,7 +3,7 @@ package dev.helw.playground.map
 import android.app.Application
 import org.maplibre.android.MapLibre
 
-class MapPlaygroundApp : Application() {
+class MapPlaygroundApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
